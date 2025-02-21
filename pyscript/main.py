d9b7fffc136525a43e050ccc76c40170d5c32555
matysekg <matysekg@poczta.onet.pl>
2025-02-21 23:26:11 +0100

    Replaced the NBP API URL to use https

--- pyscript/main.py
+++ pyscript/main.py
@@ -2,7 +2,7 @@ from pyscript import fetch
 
 response = await fetch(
 #    "https://examples.pyscriptapps.com/api-proxy-tutorial/api/proxies/status-check",
-    "http://api.nbp.pl/api/exchangerates/rates/A/USD/2020-06-29/",
+    "https://api.nbp.pl/api/exchangerates/rates/A/USD/2020-06-29/",
     method="GET"
 ).json()
 
